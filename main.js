53a502e8e372be38454724de6e9010621d48aece
bf_tsai <tsai19911026@gmail.com>
2023-08-18 18:26:41 +0800

    font family

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+//<script src="https://kit.fontawesome.com/96a7520586.js" crossorigin="anonymous"></script>
