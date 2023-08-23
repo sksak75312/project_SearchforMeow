88889186f7cb7ff2f6df19da94dffd55e504f7d1
bf_tsai <tsai19911026@gmail.com>
2023-08-23 16:29:49 +0800

    shelter building

--- assets/js/shelter.js
+++ assets/js/shelter.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //dropdown
     $('.dropBtn').click(function (e) { 
         e.preventDefault();
         $('.material-symbols-outlined').toggleClass('active');
@@ -6,28 +7,55 @@ $(document).ready(function () {
     $('.item0').click(function (e) { 
         e.preventDefault();
         $('.dropBtn').text("全部");
-        $('.dropBtn').html("<span>unfold_more</span>").addClass('material-symbols-outlined');;
-        //$( ".dropBtn" ).text( "全部 unfold_more" );
+        $('.dropBtn').append("<span>unfold_more</span>").find('span').addClass('material-symbols-outlined active');
+        setTimeout(() => {
+            $('.material-symbols-outlined').removeClass('active');
+        }, 20);
     });
     $('.item1').click(function (e) { 
         e.preventDefault();
         $( ".dropBtn" ).text( "北部" );
+        $('.dropBtn').append("<span>unfold_more</span>").find('span').addClass('material-symbols-outlined active');
+        setTimeout(() => {
+            $('.material-symbols-outlined').removeClass('active');
+        }, 20);
     });
     $('.item2').click(function (e) { 
         e.preventDefault();
         $( ".dropBtn" ).text( "中部" );
+        $('.dropBtn').append("<span>unfold_more</span>").find('span').addClass('material-symbols-outlined active');
+        setTimeout(() => {
+            $('.material-symbols-outlined').removeClass('active');
+        }, 20);
     });
     $('.item3').click(function (e) { 
         e.preventDefault();
         $( ".dropBtn" ).text( "南部" );
+        $('.dropBtn').append("<span>unfold_more</span>").find('span').addClass('material-symbols-outlined active');
+        setTimeout(() => {
+            $('.material-symbols-outlined').removeClass('active');
+        }, 20);
     });
     $('.item4').click(function (e) { 
         e.preventDefault();
         $( ".dropBtn" ).text( "東部" );
+        $('.dropBtn').append("<span>unfold_more</span>").find('span').addClass('material-symbols-outlined active');
+        setTimeout(() => {
+            $('.material-symbols-outlined').removeClass('active');
+        }, 20);
     });
     $('.item5').click(function (e) { 
         e.preventDefault();
         $( ".dropBtn" ).text( "離島" );
+        $('.dropBtn').append("<span>unfold_more</span>").find('span').addClass('material-symbols-outlined active');
+        setTimeout(() => {
+            $('.material-symbols-outlined').removeClass('active');
+        }, 20);
+    });
+    //
+    $('.favor').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('addFavor');
     });
 });
 
