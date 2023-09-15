83369d73e238d62eec5372a1daa65471f72fc90b
sksak75312 <sksak75312@gmail.com>
2023-09-15 10:37:47 +0800

    修正程式碼

--- assets/js/btnDropdown.js
+++ assets/js/btnDropdown.js
@@ -5,17 +5,14 @@ let arrowUp = null;
 btnDropdown.forEach((arrow) => {
   arrow.addEventListener('click', () => {
     const iconDown = arrow.querySelector('.js-iconDown');
-
+    iconDown.classList.remove('rotate__up')
     if (arrow === arrowUp) {
-      iconDown.classList.toggle('rotate__down');
       iconDown.classList.toggle('rotate__up');
       arrowUp = null;
     } else {
       if (arrowUp) {
-        arrowUp.querySelector('.js-iconDown').classList.toggle('rotate__down');
         arrowUp.querySelector('.js-iconDown').classList.toggle('rotate__up');
       }
-      iconDown.classList.toggle('rotate__down');
       iconDown.classList.toggle('rotate__up');
       arrowUp = arrow;
     }
